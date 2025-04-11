7aae1c18755f372ba88af467a4e568ad9c289c1b
echenim <myron.echenim@gmail.com>
2025-04-11 00:05:16 -0700

    update on dns

--- internal/dns/dns.go
+++ internal/dns/dns.go
@@ -4,67 +4,47 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/route53"
-	"github.com/sirupsen/logrus"
 )
 
-// DNSManager defines the structure for managing DNS operations
 type DNSManager struct {
-	service *route53.Route53
-	zoneID  string
+	Service *route53.Route53
 }
 
-// NewDNSManager initializes a new DNS manager instance
 func NewDNSManager(zoneID string) *DNSManager {
 	sess := session.Must(session.NewSession(&aws.Config{
-		Region: aws.String("us-west-2"), // Specify the AWS region as needed
+		Region: aws.String("us-west-2"), // or dynamically from config
 	}))
 	service := route53.New(sess)
 	return &DNSManager{
-		service: service,
-		zoneID:  zoneID,
+		Service: service,
 	}
 }
 
-// UpdateRecord sets or updates a DNS record
-func (d *DNSManager) UpdateRecord(recordName, recordType, recordValue string) error {
+func (m *DNSManager) UpdateRecord(name, recordType, value string) {
 	input := &route53.ChangeResourceRecordSetsInput{
-		HostedZoneId: aws.String(d.zoneID),
 		ChangeBatch: &route53.ChangeBatch{
 			Changes: []*route53.Change{
 				{
 					Action: aws.String("UPSERT"),
 					ResourceRecordSet: &route53.ResourceRecordSet{
-						Name: aws.String(recordName),
+						Name: aws.String(name),
 						Type: aws.String(recordType),
 						TTL:  aws.Int64(300),
 						ResourceRecords: []*route53.ResourceRecord{
 							{
-								Value: aws.String(recordValue),
+								Value: aws.String(value),
 							},
 						},
 					},
 				},
 			},
 		},
+		HostedZoneId: aws.String("ZONE_ID"),
 	}
-
-	_, err := d.service.ChangeResourceRecordSets(input)
+	_, err := m.Service.ChangeResourceRecordSets(input)
 	if err != nil {
-		logrus.WithFields(logrus.Fields{
-			"zoneID":      d.zoneID,
-			"recordName":  recordName,
-			"recordType":  recordType,
-			"recordValue": recordValue,
-			"error":       err,
-		}).Error("Failed to update DNS records")
-		return err
+		// logger.Log.WithFields(logrus.Fields{
+		// 	"name": name, "type": recordType, "value": value, "error": err,
+		// }).Error("Failed to update DNS record")
 	}
-
-	logrus.WithFields(logrus.Fields{
-		"zoneID":      d.zoneID,
-		"recordName":  recordName,
-		"recordType":  recordType,
-		"recordValue": recordValue,
-	}).Info("DNS record updated successfully")
-	return nil
 }
